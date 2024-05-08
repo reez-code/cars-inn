cd58375bcc9cbf3d7a098c6c11d9567e041433d7
Phil <phil.github@gmail.com>
2024-05-08 16:36:32 +0300

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [cars, setCars] = useState;
+  const [cars, setCars] = useState([]);
   useEffect(() => {
-    fetch();
-  });
+    fetch("http://localhost:3000/cars")
+      .then((res) => res.json())
+      .then((data) => setCars(data));
+  }, []);
+  console.log(cars);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
