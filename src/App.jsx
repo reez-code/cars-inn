23c216d96dfa059ed35f39751ca4c6c6dbc1e70e
Phil <phil.github@gmail.com>
2024-05-08 16:31:43 +0300

    json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [cars, setCars] = useState;
+  useEffect(() => {
+    fetch();
+  });
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
