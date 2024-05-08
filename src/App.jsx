e9c8c68ce6631ad873126859238a8848132cfec0
Phil <phil.github@gmail.com>
2024-05-08 16:20:27 +0300

    configured with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <div className="container py-4 px-3 mx-auto">
-      <h1>Hello, Bootstrap and Vite!</h1>
-      <button className="btn btn-primary">Primary button</button>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
