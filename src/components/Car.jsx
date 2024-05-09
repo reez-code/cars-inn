6f5f81ae0a018793c90fc35ecd3de7836e12426a
Phil <phil.github@gmail.com>
2024-05-09 10:27:43 +0300

    onclick removed

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -12,7 +12,6 @@ function Car({ image, description, title }) {
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               type="submit"
-              onClick={(e) => console.log(e)}
             >
               Add to Library
             </button>
