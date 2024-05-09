92cd0b69ba07f1924bb5b6f7e1fce7a2167756de
Phil <phil.github@gmail.com>
2024-05-09 10:29:49 +0300

    back to original

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,7 +1,4 @@
 function Car({ image, description, title }) {
-  function handleClick(e) {
-    e.preventDefault();
-  }
   return (
     <>
       <form>
@@ -15,7 +12,6 @@ function Car({ image, description, title }) {
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               type="submit"
-              onClick={(e) => handleClick(e)}
             >
               Add to Library
             </button>
