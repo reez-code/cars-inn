f22feb7cbd940c2aa514dda3a433f4fb73c6f640
Phil <phil.github@gmail.com>
2024-05-09 18:33:51 +0300

    centered form

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -21,11 +21,14 @@ function Add({ addCars }) {
       title: title,
     };
     addCars(formData);
+    setImage("");
+    setDescription("");
+    setTitle("");
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="pl-[25%]">
         <div
           className="mt-10 grid gap-x-6 gap-y-8 sm:grid-cols-8"
           id="input"
@@ -58,7 +61,7 @@ function Add({ addCars }) {
           <div className="sm:col-span-2">
             <div className="mt-2">
               <input
-                type="text"
+                type="url"
                 id="image"
                 placeholder="Insert Image URL"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
