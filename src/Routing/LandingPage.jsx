4a610d55a0a8cc1a4fc72fb8d496404249fc646b
cyndi mustapha <marrencyndi101@gmail.com>
2024-05-13 12:42:35 +0300

    adding NavBar to Library

--- src/Routing/LandingPage.jsx
+++ src/Routing/LandingPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Landing from "../components/Landing";
+import Navbar from "../components/Navbar";
+
 
 function LandingPage() {
   return (
     <div>
+      <Navbar />
       <Landing />
     </div>
   );
--- src/Routing/Library.jsx
+++ src/Routing/Library.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import {useState} from "react";
 import CarLibrary from "../components/CarLibrary";
 import { useLocation } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Library() {
   const location = useLocation();
   console.log(location);
   console.log(location.state);
 
-  
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  };
+
+  const filteredCars = location.state.filter((car) => {
+    return car.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="container mx-auto">
+      <Navbar onSearch={handleSearch} />
       <h1 className="text-3xl font-bold my-8">My Library</h1>
-
-      <CarLibrary cars={location.state} />
-      
+      <CarLibrary cars={filteredCars} />
     </div>
   );
 }
