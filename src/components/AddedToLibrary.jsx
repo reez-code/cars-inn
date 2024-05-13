2136a01c7ec7e3c64d0070916db40c89bfec140a
cyndi mustapha <marrencyndi101@gmail.com>
2024-05-13 12:24:06 +0300

    done

--- src/components/AddedToLibrary.jsx
+++ src/components/AddedToLibrary.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Car({ id, image, description, title, onAddToLibrary }) {
+function AddedToLibrary({ id, image, description, title, onAddToLibrary }) {
   const [added, setAdded] = useState(false);
 
   const handleClick = () => {
@@ -17,21 +17,10 @@ function Car({ id, image, description, title, onAddToLibrary }) {
             <div className="font-bold text-xl mb-2">{title}</div>
             <p className="text-gray-700 text-base">{description}</p>
           </div>
-          <div className="pl-6 pt-4 pb-2">
-            <button
-              className={`py-2 px-4 rounded ${
-                added ? "bg-green-500" : "bg-blue-500"
-              } hover:bg-blue-700 text-white font-bold`}
-              type="button"
-              onClick={handleClick}
-            >
-              {added ? "Added to Library" : "Add to Library"}
-            </button>
-          </div>
         </div>
       </form>
     </>
   );
 }
 
-export default Car;
+export default AddedToLibrary;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logoImage from '../images/logo-purple.png';
 import SearchBar from './Search';
+import { Link } from 'react-router-dom';
 
 
-function Navbar({onSearch}) {
+function Navbar({onSearch, addedCars}) {
   return (
     <nav>
       <div className="flex justify-between items-center bg-neutral-500 text-white p-4">
@@ -22,6 +23,11 @@ function Navbar({onSearch}) {
             Home
           </NavLink>
 
+          <NavLink
+            className="text-lg"
+          >
+            <Link to="/Library" state={addedCars}>Library</Link>
+          </NavLink>
         </div>
         <div> <SearchBar onSearch={onSearch}/> </div>
       </div>
